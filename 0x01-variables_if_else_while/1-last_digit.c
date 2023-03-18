ffd8d5461963e620311c589bda87696b42eb6b71
Macvistah <markkelvistah@gmail.com>
2023-03-17 17:01:53 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,8 @@
 
 /* betty style doc for function main goes there */
 /**
-* main - Entry point
-* Return: Always 0 (success)
+*main - Entry point
+*Return: Always 0 (success)
 */
 int main(void)
 {
