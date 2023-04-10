303c398a3193780c496085331837116ba8746f92
Macvistah <markkelvistah@gmail.com>
2023-04-10 16:11:17 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 #define MAIN_H
@@ -24,6 +23,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
-#endif 
+#endif
+#endif
